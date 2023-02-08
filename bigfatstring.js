c33aa5ded8ceec4e310bba7e60276fd8f280e1bd
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-08 19:41:25 +0100

    option 6

--- bigfatstring.js
+++ bigfatstring.js
@@ -21,7 +21,29 @@ function generate(){
         output = input.substring(0, input.indexOf(" ")).length;
     } else if (choiceValue === "3"){
         output = `${input.indexOf(" ")+1} , ${input.lastIndexOf(" ")} , ${input.substring(input.indexOf(" ")+1 , input.lastIndexOf(" "))}`;
-    } else if
+    } else if (choiceValue === "4"){
+        // Solution 1
+        // output = input.endsWith(".png") || input.endsWith(".jpg");
+        // if(output){
+        //     output = "this file is a png or jpg ";
+        // }else {
+        //     output = "this file is not a png or jpg ";
+        // }
+        // Solution 2
+        if(input.endsWith(".png")){
+            output = "this file is a png";
+        } else if(input.endsWith(".jpg")) {
+            output = "this file is a jpg";
+        } else {
+            output = "this file isn't a png or a jpg"
+        }
+        // Solution 3
+        // output = `This file is a ${input.substring(input.lastIndexOf(".")+1)}`
+    } else if (choiceValue === "5"){
+        output = "*".repeat(input.length);
+    } else if 
+
+
     console.log(output);
     document.querySelector("input#output").value = output;
 }
